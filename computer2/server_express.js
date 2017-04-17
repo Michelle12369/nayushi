1ea79fe9f42d267c7d67d83ee21ff86ee7d2b7d1
Michelle <michelle@Michellede-MacBook-Pro.local>
2017-04-17 18:25:33 +0800

    computer2 edit

--- computer2/server_express.js
+++ computer2/server_express.js
@@ -1,10 +1,11 @@
+'use strict'
 // require
-var express = require('express')
-  , io = require('socket.io')
-  , http = require('http');
+var express = require('express'),
+    io = require('socket.io'),
+    http = require('http');
 
 // express define
-var app = express(); 
+var app = express();
 var router = express.Router();
 var server = http.createServer(app);
 
@@ -15,51 +16,60 @@ app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
 //routes settings
-router.get('/',function(req,res,next){
-	res.render('index');
+router.get('/', function (req, res, next) {
+    res.render('index');
 });
 
 app.use('/', router);
 
 // socket
 var listener = io.listen(server);
-var clients = [];
+var clientStatus = [];
 listener.sockets.on('connection', function (socket) {
     // example
-    console.log("connection "+socket.request.connection.remoteAddress);
-    clients.push(socket);
-   
-    socket.emit('message', { //send message to client
-        'message': 'Hello client, I am client.'
-    }); 
-    socket.on('news', function (data) { //get news from client
-        console.log(data.hello);
-    });
-    socket.on('room',function(data){
+    // socket.emit('message', { //send message to client
+    //     'message': 'Hello client, I am client.'
+    // });
+    // socket.on('news', function (data) { //get news from client
+    //     console.log(data.hello);
+    // });
+    socket.on('room', function (data) {
         socket.join(data.room);
-        console.log(data.room);
+        console.log(`${data.room} has joined the room`);
     })
 
     //real code: computer3
-    var com3_status=false;
-    var com3_finished=false;
-    socket.in("localclient").on('forCom3', function (data) { 
-        if(!com3_status){
-            console.log(data.sense);
-            socket.in("web").emit('playVideo3',{
-                'playVideo3':1
+    var com3_status = false;
+    socket.in("localclient").on('forCom3', function (data) {
+        if (!com3_status) {
+            // console.log(data.sense);
+            socket.in("web").emit('playVideo3', {
+                'playVideo3': 1
             })
-            // clients[clients.length-1].emit('playVideo3',{
-            //     'playVideo3':1
-            // })
-            com3_status=true;
+            com3_status = true;
         }
     });
-    socket.in("web").on('computer3_end',function(data){
-        if(data.computer3_end==true){
-            com3_finished=true;
+
+    //check whether other computer has finished
+    socket.in("web").on('computer3Finished', function (data) {
+        console.log(`com3:${data.computer3Finished}`);
+        if (data.computer3Finished == true) {
+            clientStatus.push("computer3 finished");
         }
     });
+    socket.in('computer1').on('computer1Finished', function (data) {
+        console.log(`com1:${data.computer1Finished}`);
+        if (data.computer1Finished == true) {
+            clientStatus.push("computer1 finished");
+        }
+    });
+    socket.in('computer4').on('computer4Finished', function (data) {
+        console.log(`com4:${data.computer4Finished}`);
+        if (data.computer4Finished == true) {
+            clientStatus.push("computer4 finished");
+        }
+    });
+
 });
 
 server.listen(3002);
--- computer3/arduino.js
+++ computer3/arduino.js
@@ -3,7 +3,7 @@ var five = require("johnny-five"),
   fsr;
 
 var socket = io.connect('http://localhost:3002');
-(new five.Board()).on("ready", function() {
+(new five.Board()).on("ready", function () {
 
   // Create a new `fsr` hardware instance.
   fsr = new five.Sensor({
@@ -12,14 +12,14 @@ var socket = io.connect('http://localhost:3002');
   });
 
   // Scale the sensor's value to the LED's brightness range
-  fsr.scale([0, 255]).on("data", function() {
+  fsr.scale([0, 255]).on("data", function () {
 
     console.log(this.scaled);
-    if(this.scaled<=200){
-        socket.emit('forCom3', {//send news to server
-            sense: 1
-        }); 
-        
+    if (this.scaled <= 200) {
+      socket.emit('forCom3', { //send news to server
+        sense: 1
+      });
+
     }
   });
 });
