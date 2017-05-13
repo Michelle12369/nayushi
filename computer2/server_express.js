5695cd747981cfa8ec0ec152a00630920c412e7b
michelle12369 <p5i7gaaaa@gmail.com>
2017-05-13 12:05:56 +0800

    computer2

--- computer2/server_express.js
+++ computer2/server_express.js
@@ -62,13 +62,13 @@ listener.sockets.on('connection', function (socket) {
     //get computer3 makey makey
     var com3_status = false;
     socket.in("computer3").on('forCom3', function (data) {
-        // if (!com3_status) {
+        if (!com3_status) {
             console.log("get makey:"+data.sense);
             socket.in("web").emit('playVideo3', {
                 'playVideo3': 1
             });
             com3_status = true;
-        // }
+        }
     });
     // socket.in("localclient").on('shortcut',function(data){
     //     console.log('got it');
